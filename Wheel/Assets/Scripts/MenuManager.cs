bc78d8284ac445e30ffd0402117358a6daf5b081
Aditya Kumar <adityaadi111203@gmail.com>
2026-06-27 14:46:10 +0530

    idk

--- Wheel/Assets/Scripts/MenuManager.cs
+++ Wheel/Assets/Scripts/MenuManager.cs
@@ -6,7 +6,7 @@ public class MenuManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Screen.fullScreen = true;
     }
 
     // Update is called once per frame
